f63793b6735e45cf669cb8c2471c4a382dc1e3e1
Rtollinchi <rtollinchi23@gmail.com>
2023-03-03 17:26:57 -0500

    exported pwd.js  and added require to bash.js

--- bash.js
+++ bash.js
@@ -1,10 +1,5 @@
-//Output a prompt
-process.stdout.write('prompt > ');
+const pwd = require('./pwd');
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === "pwd"){ 
-    process.stdout.write(process.cwd())
-    process.stdout.write('\nprompt > ');}
- }) 
+pwd()
+
 
